89cad61eab46677e04eca166b84d79264883f1d9
Hashir Asad <145664273+WeWeBunnyX@users.noreply.github.com>
2024-08-03 20:42:49 +0500

    innit

---
+++ main.py
@@ -0,0 +1,15 @@
+import flet as ft
+from flet import * 
+
+def main(page:Page):
+    page.title='Youtube Downloader'
+    page.window.width=500
+    page.window.height=650
+
+
+    page.add()
+
+
+
+
+app(target=main)
